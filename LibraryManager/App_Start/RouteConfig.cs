6643d3c566f704dadaf3c04a45c0ca32496f32e5
Nikita Sakhno <adream74@gmail.com>
2016-06-23 01:03:58 +0500

    Изменил стандартный роут

--- LibraryManager/App_Start/RouteConfig.cs
+++ LibraryManager/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace LibraryManager
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
