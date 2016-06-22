1ae96099b7b81feed17efb7fa4c8b7aa9d44a794
Nikita Sakhno <adream74@gmail.com>
2016-06-22 23:44:18 +0500

    Empty Home Controller

---
+++ LibraryManager/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace LibraryManager.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello world");
+        }
+    }
+}
